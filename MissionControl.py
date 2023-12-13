25b8a7b751d185b69ada31de66cd68aae1ef3f2a
Zanifania <125196995+Zanifania@users.noreply.github.com>
2023-12-13 23:20:52 +0100

    Update MissionControl.py

--- MissionControl.py
+++ MissionControl.py
@@ -1,23 +1,36 @@
 import random as rand
 from DroneManager import DroneManager
 from RoverManager import RoverManager
+import pygame
+import Assets
 
 class MissionControl():
     def __init__(self, game):
+        
         # Set the seed from the settings
         rand.seed(game.sim_settings[1])
 
         self.game = game
+        self.settings = game.sim_settings
         self.cave_gen = self.game.cave_gen
         self.cave_map = self.cave_gen.bin_map
-        self.settings = game.sim_settings
-
+        self.surface= game.display
+        
+        self.original_drone = pygame.image.load(Assets.Images['DRONE'].value)
+        self.drone = pygame.transform.scale(self.original_drone, (10,10))
+        
         # Initialise the Managers (and the robots)
-        self.drone_manager = DroneManager(game, self.settings[2])
-        self.rover_manager = RoverManager(game)
-
+        #self.drone_manager = DroneManager(game, self.settings[2])
+        #self.rover_manager = RoverManager(game)
+        
+        # Create a list to store the rectangles for each drone
+        self.drone_rects = []
+        
         # Find a suitable start position
         self.set_start_pos()
+        
+        # Create the drones
+        self.drone_gen()
         
 
     # Among the starting positions of the worms, find one that is viable
@@ -27,8 +40,49 @@ class MissionControl():
             
             # Take one of the initial points of the worms as initial point for the drone
             i = rand.randint(0,3)
-            self.initial_point = [self.cave_gen.worm_x[i],self.cave_gen.worm_y[i]]
+            self.initial_point = (self.cave_gen.worm_x[i],self.cave_gen.worm_y[i])
             
             # Check if the point is white or black
             if self.cave_map[self.initial_point[1]][self.initial_point[0]] == 0:  # White
                 good_point = True
+                
+        print("the point is", self.initial_point) 
+                
+    # Generate a new drone rect for each drone
+    def drone_gen(self):
+        nbr_drone = self.settings[2]
+        print("the nbr of drones is", nbr_drone)
+
+        for _ in range(nbr_drone):
+            # Create a new drone rect for each drone
+            drone_rect = self.drone.get_rect()
+
+            # Set the center of the drone_rect to the initial point
+            drone_rect.center = self.initial_point
+
+            # Add the new drone_rect to the list
+            self.drone_rects.append(drone_rect)
+
+        # Draw all drones on the game window
+        self.draw_drones()
+            
+    # Actually draw all the drones
+    def draw_drones(self):
+        
+        self.game.blit_screen()
+        
+        background_image = pygame.image.load(Assets.Images['CAVE_MAP'].value)
+        # Create a temporary surface with the background
+        temp_surface = background_image.copy()
+        
+        self.surface.blit(self.drone, (0, 0))
+        
+        # Draw all drones on the temporary surface
+        for drone_rect in self.drone_rects:
+            temp_surface.blit(self.drone, drone_rect.center)  
+
+        # Blit the temporary surface onto the game window
+        self.surface.blit(temp_surface, (0, 0))
+
+        # Update the display
+        pygame.display.update()

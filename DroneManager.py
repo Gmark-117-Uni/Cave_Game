db892311cced39cc31d80628b261b8076ed91ebc
Zanifania <125196995+Zanifania@users.noreply.github.com>
2023-12-15 18:23:57 +0100

    Update DroneManager.py

--- DroneManager.py
+++ DroneManager.py
@@ -1,11 +1,18 @@
-from Drone import Drone
+import pygame
+import random as rand
+import numpy as np
+import MapGenerator as MapGenerator
+
 
 class DroneManager():
-    def __init__(self, game, n_drones):
+    def __init__(self, game, point, colors):
         self.game = game
-        self.cave = game.cave_gen.bin_map
-        self.drone_list = self.build_drones(n_drones)
-
-    def build_drones(self, n):
-        pass
-
+        self.initial_point = point
+        self.step = 30
+        self.next_point = self.next_step()
+        print('drones point' + str(self.initial_point))
+        
+    def next_step(self):
+    
+       self.next_point = (self.initial_point[0] + self.step, self.initial_point[1])
+       return self.next_point

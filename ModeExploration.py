b2933d5aca874bd49043775ffcfa2266526a3f98
Zanifania <125196995+Zanifania@users.noreply.github.com>
2023-12-18 17:55:16 +0100

    Update ModeExploration.py

--- ModeExploration.py
+++ ModeExploration.py
@@ -5,8 +5,10 @@ class ModeExploration:
         self.start_point = start_point
         self.num_drones = num_drones
         # Delay in milliseconds 
-        self.delay = 0
-         
+        self.delay = 300
+        # Initialize your class attributes here
+        self.stop_movement_flag = False
+        self.step= 0
         
     def next_point(self):
         self.next_point = []  
@@ -15,4 +17,12 @@ class ModeExploration:
             point = self.start_point[i] 
             self.tmp_next_point = (point[0]-i*10, point[1] - 10)
             self.next_point.append(self.tmp_next_point)
+        self.step += 1
+        if self.step > 10:
+            self.stop_movement_flag = True
+            self.finish_exploration()
         return self.next_point
+    
+    def finish_exploration(self):
+        
+        return self.stop_movement_flag

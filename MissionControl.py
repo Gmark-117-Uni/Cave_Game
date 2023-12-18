ade5b032913ee79eed0a851a468250fcfa59d5ad
Gmark-117-Uni <gianmarco.lavacca@studenti.unitn.it>
2023-12-18 16:27:08 +0100

    Zanifania MissionControl.py update

--- MissionControl.py
+++ MissionControl.py
@@ -13,8 +13,12 @@ class MissionControl():
         self.cave_gen = self.game.cave_gen
         self.cave_map = self.cave_gen.bin_map
         self.cave_png = Assets.Images['CAVE_MAP'].value
+        self.black_cave_png = Assets.Images['BLACK_CAVE_MAP'].value
         self.surface= game.display
         
+        # Save the borders
+        self.save_black_mask()
+
         # Find a suitable start position
         self.set_start_pos()
         
@@ -39,6 +43,23 @@ class MissionControl():
         while True:
             self.fleet.move_drones()
             self.clear_previous_drones()
+    
+    def save_black_mask (self):
+        # Load the CAVE_MAP image
+        cave_map = pygame.image.load(self.cave_png).convert_alpha()
+        # Create a new surface with per-pixel alpha
+        modified_cave_map = pygame.Surface(cave_map.get_size(), pygame.SRCALPHA)
+        # Iterate through each pixel and clear black pixels
+        for y in range(cave_map.get_height()):
+            for x in range(cave_map.get_width()):
+                # Get the color of the current pixel
+                pixel_color = cave_map.get_at((x, y))
+                if  pixel_color == (255, 255, 255, 255):  
+                    pixel_color = (0, 0, 0, 0)  
+                # Set the pixel color in the modified surface
+                modified_cave_map.set_at((x, y), pixel_color)
+        # Save the modified map
+        pygame.image.save(modified_cave_map, Assets.Images['BLACK_CAVE_MAP'].value)
 
     # Among the starting positions of the worms, find one that is viable
     def set_start_pos(self):
